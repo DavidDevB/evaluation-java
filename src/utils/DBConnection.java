505fb59f0e44bf1a6228df8e922582481b23e3e5
DavidB <david.bernard@fms-ea.com>
2026-01-05 15:53:18 +0100

    Java strings added to DBConnection

--- src/utils/DBConnection.java
+++ src/utils/DBConnection.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
-    private static final String USER = "your_username";
-    private static final String PASSWORD = "your_password";
+    // Détails de la connexion à la base de données
+    // Remplacez par vos propres informations de connexion
+    // Assurez-vous que le pilote JDBC pour MySQL est dans le classpath
+    // URL de connexion, utilisateur et mot de passe
+
+    private static final String URL = "jdbc:mysql://localhost:3306/formation";
+    private static final String USER = "formation";
+    private static final String PASSWORD = "gS4_)xp9!qUosghv";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
