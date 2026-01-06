9d43eee1ca4f831670265e0c64f1613580600d96
DavidB <david.bernard@fms-ea.com>
2026-01-06 10:30:45 +0100

    model Cart modified to fit model Course

--- src/models/Cart.java
+++ src/models/Cart.java
@@ -1,38 +1,26 @@
 package models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Cart {
 
-    private String[] articles;
+    private final List<Course> articles;
 
-    public Cart(String[] articles) {
+    public Cart(List<Course> articles) {
         this.articles = articles;
     }
 
-    public void addArticle(String courseName) {
-        String[] newArticles = new String[articles.length + 1];
-        System.arraycopy(articles, 0, newArticles, 0, articles.length);
-        newArticles[articles.length] = courseName;
-        articles = newArticles;
+    public void addArticle(Course course) {
+        articles.add(course);
     }
 
     public void removeArticle(String courseName) {
-        int index = -1;
-        for (int i = 0; i < articles.length; i++) {
-            if (articles[i].equals(courseName)) {
-                index = i;
-                break;
-            }
-        }
-        if (index != -1) {
-            String[] newArticles = new String[articles.length - 1];
-            System.arraycopy(articles, 0, newArticles, 0, index);
-            System.arraycopy(articles, index + 1, newArticles, index, articles.length - index - 1);
-            articles = newArticles;
-        }
+        articles.removeIf(course -> course.getName().equalsIgnoreCase(courseName));
     }
 
     // Getter
-    public String[] getArticles() {
+    public List<Course> getArticles() {
         return articles;
     }
 }
